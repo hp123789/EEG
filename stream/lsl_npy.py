b719826a5a603f67d97164ae75c88ea67a487cfe
hp123789 <45138135+hp123789@users.noreply.github.com>
2020-04-05 00:11:38 -0700

    Add files via upload

---
+++ stream/lsl_npy.py
@@ -0,0 +1,19 @@
+from pylsl import StreamInlet, resolve_stream
+import time
+import numpy as np
+
+streams = resolve_stream('type', 'EEG')
+
+data = []
+t_end = time.time() + 7
+
+inlet = StreamInlet(streams[0])
+
+while time.time() < t_end:
+    sample, timestamp = inlet.pull_sample()
+    if sample:
+        data.append(sample)
+        if len(data) > 1250:
+            data.pop(0)
+            a = np.array(data)
+            np.save("eegdata", a)
